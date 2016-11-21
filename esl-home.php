cf8cc9870e90c4de6b755091a76792aae1f3dc32
Nate Finch <finchproservices@gmail.com>
2016-11-21 16:42:46 +0900

    initial setup for theme and homepage

---
+++ esl-home.php
@@ -0,0 +1,55 @@
+<?php
+
+//* Template Name: ESL Home
+
+/**
+ * Description
+ *
+ * @package     $NAMESPACE
+ * @since       1.0.0
+ * @author      n8finch
+ * @link        https://n8finch.com
+ * @license     GNU General Public License
+ */
+
+
+
+//* Force full width content layout
+add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
+// Remove the default Genesis loop
+remove_action( 'genesis_loop', 'genesis_do_loop' );
+// Add custom homepage content
+add_action( 'genesis_loop', 'esl_homepage_content' );
+function esl_homepage_content() {
+	?>
+	<section id="homepage-main-section">
+		<div id="home-search-bar">
+			<p>Search Bar will go here...</p>
+		</div>
+
+		<div id="home-teacher-intro">
+			<p><img src="wp-content/themes/esl-theme/images/teacher-stick-figure-transparent.png"/></p>
+			<h2>Where do you want to teach?</h2>
+		</div>
+
+		<div id="home-location-options">
+			<div class="home-location-option-boxes">
+				<span>Korea</span>
+			</div>
+
+			<div class="home-location-option-boxes">
+				<span>China</span>
+			</div>
+
+			<div class="home-location-option-boxes">
+				<span>Anywhere</span>
+			</div>
+		</div>
+
+
+	</section>
+	<?php
+}
+
+//* Run the Genesis loop
+genesis();
--- functions.php
+++ functions.php
@@ -114,3 +114,12 @@ function genesis_sample_comments_gravatar( $args ) {
 	return $args;
 
 }
+
+
+
+//* Customize the entire footer
+add_filter('genesis_footer_creds_text', 'sp_footer_creds_filter');
+function sp_footer_creds_filter( $creds ) {
+	$creds = '[footer_copyright] - All rights reserved &middot; <a href="http://mydomain.com">ESL Theme</a> &middot; Built on the <a href="http://www.studiopress.com/themes/genesis" title="Genesis Framework">Genesis Framework</a>';
+	return $creds;
+}
