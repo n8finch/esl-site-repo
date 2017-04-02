966d395cc7cffcc8ef7c8499609d9df6e653ee27
Nate Finch <finchproservices@gmail.com>
2017-04-02 16:11:09 +0900

    start new homepage features

--- esl-home.php
+++ esl-home.php
@@ -18,9 +18,20 @@
 add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
 // Remove the default Genesis loop
 remove_action( 'genesis_loop', 'genesis_do_loop' );
+
+
 // Add custom homepage content
-add_action( 'genesis_loop', 'esl_homepage_content' );
-function esl_homepage_content() {
+add_action( 'genesis_loop', 'esl_homepage_top_content' );
+
+
+/**************************************************
+ * ESL HOMEPAGE CONTENT
+ **************************************************/
+
+/*
+ * ESL Hompage Main
+ */
+function esl_homepage_top_content() {
 	?>
 	<section id="homepage-main-section">
 		<div id="home-search-bar">
