c2d30539905a77791b975ef57e6f10c79c43e179
Nate Finch <finchproservices@gmail.com>
2017-05-05 14:09:12 +0900

    Added templates, updated header styles slightly.

--- single-job-postings.php
+++ single-job-postings.php
@@ -5,15 +5,15 @@
  */
 
 
-remove_action( 'genesis_loop', 'genesis_do_loop');
-add_action( 'genesis_loop', 'esl_custom_post' ); // Add custom loop
-
-function esl_custom_post() {
-
-	global $post;
-	// d($post);
-	// d(get_post_meta($post->ID));
-	the_meta($post->ID);
-}
+// remove_action( 'genesis_loop', 'genesis_do_loop');
+// add_action( 'genesis_loop', 'esl_custom_post' ); // Add custom loop
+//
+// function esl_custom_post() {
+//
+// 	global $post;
+// 	// d($post);
+// 	// d(get_post_meta($post->ID));
+// 	the_meta($post->ID);
+// }
 
 genesis();
