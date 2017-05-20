f5e13262d45bb4299f1233fd2e27973c5cc1e782
Nate Finch <finchproservices@gmail.com>
2017-05-20 14:46:14 +0900

    updated styling on homepage and interior pages

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,40 @@
 (function($) {
 
-	$(document).ready( function() {
-
-		// var homeFilterOptions = $('#home-filter-options');
-
-		// $(homeFilterOptions).hide();
-		//
-		// $('.home-location-option-boxes').on('click', function() {
-		// 	$(homeFilterOptions).slideToggle();
-		// });
-
+    $(document).ready(function() {
 
 		$('#apply-now-button').on('click', function() {
-			$('#hidden-application-form').slideDown();
-		});
+            $('#hidden-application-form').slideToggle();
+        });
+
+        // var homeFilterOptions = $('#home-filter-options');
+
+        // $(homeFilterOptions).hide();
+        //
+        // $('.home-location-option-boxes').on('click', function() {
+        // 	$(homeFilterOptions).slideToggle();
+        // });
 
 
-	});
+		//
+        // $("#bookmarkme").click(function(e) {
+        //     e.preventDefault(); // this will prevent the anchor tag from going the user off to the link
+        //     var bookmarkUrl = this.href;
+        //     var bookmarkTitle = this.title;
+		//
+        //     if (window.sidebar) { // For Mozilla Firefox Bookmark
+        //         window.sidebar.addPanel(bookmarkTitle, bookmarkUrl, "");
+        //     } else if (window.external || document.all) { // For IE Favorite
+        //         window.external.AddFavorite(bookmarkUrl, bookmarkTitle);
+        //     } else if (window.opera) { // For Opera Browsers
+        //         $("a.jQueryBookmark").attr("href", bookmarkUrl);
+        //         $("a.jQueryBookmark").attr("title", bookmarkTitle);
+        //         $("a.jQueryBookmark").attr("rel", "sidebar");
+        //     } else { // for other browsers which does not support
+        //         alert('Your browser does not support this bookmark action');
+        //         return false;
+        //     }
+        // });
+
+    });
 
 })(jQuery);
